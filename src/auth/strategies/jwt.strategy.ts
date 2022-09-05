d447fb4a2b1f9d28044e576e11c98c53b30a0511
samuel <samuel@kali.kali.net>
2022-09-05 11:13:41 -0600

    passport strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { Repository } from "typeorm";
+
+import { Admin } from "../entities/admin.entity";
+import { JwtPayload } from "../interfaces/jwt-payload.interface";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(Admin)
+    private readonly adminRepository: Repository<Admin>,
+
+    configService: ConfigService
+  ) {
+    super({
+      secretOrKey: configService.get("SECRET_KEY"),
+      jwtfromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<Admin> {
+    const { id } = payload;
+
+    const admin = await this.adminRepository.findOne({
+      where: { id },
+    });
+
+    return;
+  }
+}
--- src/pet/dto/index.ts
+++
@@ -1,2 +0,0 @@
-export { CreatePetDto } from './create-pet.dto';
-export { UpdatePetDto } from './update-pet.dto';
